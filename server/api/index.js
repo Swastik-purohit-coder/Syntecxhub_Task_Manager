6ff31480fcfd9d0ef18e55d53123483120df4faa
swastik <swastikpurohit2007@gmail.com>
2026-04-19 18:17:55 +0530

    solve deploy issue

--- server/api/index.js
+++ server/api/index.js
@@ -8,8 +8,31 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_2,
+  "http://localhost:5173",
+  "http://localhost:4173",
+].filter(Boolean);
+
+const isAllowedOrigin = (origin) => {
+  if (!origin) return true;
+  if (allowedOrigins.includes(origin)) return true;
+  return /^https:\/\/[a-z0-9-]+\.vercel\.app$/i.test(origin);
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (isAllowedOrigin(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Origin not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API running...");
